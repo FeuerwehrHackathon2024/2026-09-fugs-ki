c7ab0bf5084ba774a30b0748a63ae83244849281
Alexander Lottes <alex@Mac.fritz.box>
2026-04-17 18:11:52 +0200

    Add first geotools

--- tools/main.py
+++ tools/main.py
@@ -1,7 +1,7 @@
 from mcp.server.fastmcp import FastMCP
 
 from generic import register_generic_tools
-
+from geotools import register_geo_tools
 
 mcp = FastMCP("Tools", json_response=True)
 
@@ -10,14 +10,13 @@ mcp = FastMCP("Tools", json_response=True)
 def hallo() -> str:
     return "Hello from tools!"
 
-
-@mcp.tool("add")
-def add(a: int, b: int) -> int:
-    return a + b
-
-
 register_generic_tools(mcp)
+register_geo_tools(mcp)
 
 
 if __name__ == "__main__":
+    from geotools import distance_wgs84_geodesic
+    #d = distance_wgs84_geodesic(52.52042741543588, 13.405782162596813, 52.52074696727995, 13.406442521399553)
+    #print(f"Distance between Munich and Berlin: {d:.2f} meters")
     mcp.run(transport="streamable-http")
+
